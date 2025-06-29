18c0b7246dba20dac28acfb3e3d851c08a99db2e
SV-1411 <shivansh1411@gmail.com>
2025-06-29 19:06:12 +0530

    cygnus

--- src/pages/Cygnus.tsx
+++ src/pages/Cygnus.tsx
@@ -101,7 +101,7 @@ const Cygnus: React.FC = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.4 }}
-              className="mt-8 sm:mt-16 px-4"
+              className="mt-8 sm:mt-8 px-4"
             >
               <img 
                 src="/assets/unnamed.png" 
